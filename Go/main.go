1f60a6557a2b3230045abf4f8046742b6d6ccfd7
Dax-000 <CiderBinderBunny@yandex.ru>
2024-01-17 21:40:23 +0300

    Добавить хендлеры для greet и history

--- Go/main.go
+++ Go/main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Printf("Hello Word")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/greet", greetHandler)
+	mux.HandleFunc("/greet/history", historyHandler)
+
+	s := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	s.ListenAndServe()
+}
+
+func greetHandler(res http.ResponseWriter, req *http.Request) {
+	data := []byte("Привет от Go!")
+	res.WriteHeader(200)
+	res.Write(data)
+}
+
+func historyHandler(res http.ResponseWriter, req *http.Request) {
+	data := []byte("Здесь должна быть история")
+	res.WriteHeader(200)
+	res.Write(data)
 }
