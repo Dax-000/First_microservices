c0c530b225a4b4ad324379082aab97842473d349
Dax-000 <CiderBinderBunny@yandex.ru>
2024-01-18 19:53:40 +0300

    Py файлы перемещены в папку src

---
+++ Python/src/config.py
@@ -0,0 +1,11 @@
+# конфиги postgresql
+host = "localhost"
+port = 5432
+dbname = "postgres"
+user = "postgres"
+password = "wertyxar665"
+
+# конфиги приложения
+app_host = "127.0.0.1"
+app_port = 8000
+app_workers = 3
