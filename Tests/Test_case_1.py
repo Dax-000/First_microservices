da5fc5738cbf8537935a8040a2caaf1b5852bc08
Dax-000 <CiderBinderBunny@yandex.ru>
2024-01-23 20:12:04 +0500

    Фикс питон приложения

--- Tests/Test_case_1.py
+++ Tests/Test_case_1.py
@@ -12,7 +12,7 @@ def test_py_greeting(browser, logger):
     py_greet = ServicePy(browser, logger)
     py_greet.go_to_url("http://localhost:8080/greet")
     answer = py_greet.endpoint_greet()
-    assert answer == '"Привет, None от Python!"'
+    assert answer == '"Привет, noname от Python!"'
 
     name = "Василиса"
     py_greet.go_to_url(f"http://localhost:8080/greet?name={name}")
