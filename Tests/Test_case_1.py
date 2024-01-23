65615369e46de9dffbdb5c158a6c6bed2e88a8ec
Dax-000 <CiderBinderBunny@yandex.ru>
2024-01-23 10:56:22 +0500

    Добавить тест эндпоинтов Go приложения

--- Tests/Test_case_1.py
+++ Tests/Test_case_1.py
@@ -1,12 +1,14 @@
 from pages.ServicePy import ServicePy
+from pages.ServiceGo import ServiceGo
 from json import loads
 from datetime import datetime
 
 
 date_fmt = '%d-%m-%Y %H:%M:%S.%f'
+date_fmt_go = '%d-%m-%Y %H:%M:%S'
 
 
-def test_greeting(browser, logger):
+def test_py_greeting(browser, logger):
     py_greet = ServicePy(browser, logger)
     py_greet.go_to_url("http://localhost:8080/greet")
     answer = py_greet.endpoint_greet()
@@ -18,7 +20,7 @@ def test_greeting(browser, logger):
     assert answer == f'"Привет, {name} от Python!"'
 
 
-def test_greet_history(browser, logger):
+def test_py_greet_history(browser, logger):
     py_history = ServicePy(browser, logger)
     name = "test"
     py_history.go_to_url(f"http://localhost:8080/greet?name={name}")
@@ -29,3 +31,29 @@ def test_greet_history(browser, logger):
     answer_time = datetime.strptime(d_answer['date'], date_fmt)
     time_diff = datetime.now() - answer_time
     # assert time_diff.seconds < 3 # Время в контейнере различается на 6 часов
+
+
+def test_go_greeting(browser, logger):
+    go_greet = ServiceGo(browser, logger)
+    go_greet.go_to_url("http://localhost:8081/greet")
+    answer = go_greet.endpoint_greet()
+    assert answer == 'Привет от Go!'
+
+
+def test_go_greet_history(browser, logger):
+    go_history = ServiceGo(browser, logger)
+    go_history.go_to_url("http://localhost:8081/greet")
+    go_history.go_to_url("http://localhost:8081/greet/history")
+    answer = go_history.endpoint_greet_history()
+    d_answer = loads(answer)[-1]
+    answer_time = datetime.strptime(d_answer['date'], date_fmt_go)
+    time_diff = datetime.now() - answer_time
+    # assert time_diff.seconds < 3 # Время в контейнере различается на 5 часов
+
+
+def test_go_greet_py(browser, logger):
+    go_greet = ServiceGo(browser, logger)
+    go_greet.go_to_url("http://localhost:8081/greet/python")
+    answer = go_greet.endpoint_greet_python()
+    assert answer == '"Привет, Go от Python!"'
+
---
+++ Tests/pages/ServiceGo.py
@@ -0,0 +1,20 @@
+from BaseApp import BasePage
+from selenium.webdriver.common.by import By
+
+
+class Locators:
+    ANSWER = (By.TAG_NAME, "pre")
+
+
+class ServiceGo(BasePage):
+    def endpoint_greet(self):
+        answer = self.find_element(Locators.ANSWER).text
+        return answer
+
+    def endpoint_greet_history(self):
+        answer = self.find_element(Locators.ANSWER).text
+        return answer
+
+    def endpoint_greet_python(self):
+        answer = self.find_element(Locators.ANSWER).text
+        return answer
